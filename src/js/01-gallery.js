7f98c74eb6b01ef382be1c9fc7b3373e737ce588
YuliaPysarenko <y.pisarenko@ukr.net>
2023-04-08 16:51:45 +0300

    homework

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+
+
+const galleryEl = document.querySelector(`.gallery`);
+const galleryItemsEl = createdGalleryItemsEl(galleryItems);
+
+galleryEl.insertAdjacentHTML(`beforeend`, galleryItemsEl);
+ 
+function createdGalleryItemsEl(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+   return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+    }).join(``);
+}
+
+
+console.log(galleryItemsEl);
+
+const lightbox = new SimpleLightbox('.gallery li a', {
+  captionsData: `alt`,
+  captionSelector: `img`,
+  captionType: 'img alt="${description}"',
+  captionPosition:`bottom`,
+  captionDelay: 250,
+});
+
+
+
+    
+    
