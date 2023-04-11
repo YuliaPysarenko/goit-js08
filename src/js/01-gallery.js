520c4719fa12f42d677c6d5ad979c6da06522e8f
YuliaPysarenko <y.pisarenko@ukr.net>
2023-04-11 14:31:34 +0300

    homework

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,11 @@
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+// import `../css/common.css`;
+
 console.log(SimpleLightbox);
 // Change code below this line
 
