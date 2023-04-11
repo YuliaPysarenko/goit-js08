c2bd372e66ed7127136a62944490e2103797aba3
YuliaPysarenko <y.pisarenko@ukr.net>
2023-04-11 12:47:20 +0300

    homework

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 
 import *as galleryOne from `./js/01-gallery`;
 console.log(galleryOne);
+
+import *as galleryVideo from `./js/02-video`;
+console.log(galleryVideo);
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import LodashThrottle from `lodash.throttle`;
 
+
+const form = document.querySelector(`.feedback-form`);
+// const formInput = document.querySelector(`.feedback-form  input`);
+// const formTextarea = document.querySelector(`.feedback-form textarea`);
+
+
+form.addEventListener(`submit`, onSubmitForm);
+form.addEventListener(`input`,throttle(onAllForm, 500));
+
+
+const STORAGE_KEY = `feedback-form-state`; 
+const ObjectForm = {};
+
+populateTextareaInput() 
+
+function onSubmitForm(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onAllForm(evt) {
+    const message = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, message);
+
+    const formImailMessage = ObjectForm[evt.target.name] = evt.target.value;
+    const stringifyObjectForm = JSON.stringify(formImailMessage);
+    const parseObjectForm = JSON.parse(stringifyObjectForm );
+
+    console.log(parseObjectForm)
+}
+
+function populateTextareaInput() {
+    const saveMessage = localStorage.getItem(STORAGE_KEY);
+    if (saveMessage) {
+        form.value = saveMessage;
+    }
+}
